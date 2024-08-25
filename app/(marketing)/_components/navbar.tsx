ea88fcb0f182bde9871b45ad22ba1a3b043d0018
kavyashar2 <166548973+kavyashar2@users.noreply.github.com>
2024-08-25 23:12:38 +0530

    Sixth commit

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -6,14 +6,46 @@ import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
 
 import { useConvexAuth } from "convex/react";
-import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { SignInButton, UserButton, useUser } from "@clerk/clerk-react";
 import { Button } from "@/components/ui/button";
 import { Spinner } from "@/components/spinner";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export const Navbar = () => {
   const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
+
+  const { isSignedIn, isLoaded, user } = useUser();
+  const [isAuthorized, setIsAuthorized] = useState(true);
+
+  useEffect(() => {
+    if (isLoaded && isSignedIn) {
+      const allowedDomain = "@scu.edu";
+      const emailAddresses = user?.emailAddresses || [];
+      const isValidEmail = emailAddresses.some((emailObj) =>
+        emailObj.emailAddress.endsWith(allowedDomain),
+      );
+
+      setIsAuthorized(isValidEmail);
+    }
+  }, [isSignedIn, isLoaded, user]);
+
+  if (isLoaded && isSignedIn && !isAuthorized) {
+    return (
+      <div
+        className={cn(
+          "z-20 bg-background fixed top-0 flex items-center w-full p-6",
+          scrolled && "border-b shadow-sm",
+        )}
+      >
+        <div className="ml-4 flex-1" />
+        <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+          <p className="text-red-700 font-medium">Access Denied</p>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div

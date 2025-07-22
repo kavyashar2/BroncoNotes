ea2cf56648eff949a26f3b87f7ddb83c0bd33902
kavyashar2 <166548973+kavyashar2@users.noreply.github.com>
2025-07-21 20:57:23 -0700

    Seventh commit

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -8,38 +8,43 @@ import { useParams } from "next/navigation";
 import { Title } from "./title";
 
 interface NavbarProps {
-    isCollapsed: boolean;
-    onResetWidth: () => void;
-};
-
-export const Navbar = ({
-    isCollapsed,
-    onResetWidth
-}: NavbarProps) => {
-    const params = useParams();
-
-    const document = useQuery(api.documents.getById, {
-        documentId: params.documentId as Id<"documents">,
-    });
-
-    if (document === undefined) {
-        return <p>Loading...</p>;
-    }
-
-    if (document === null){
-        return null;
-    }
-
-    return ( 
-        <>
-            <nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center gap-x-4">
-                {isCollapsed && (
-                    <MenuIcon role="button" onClick={onResetWidth} className="h-6 w-6 text-muted-foreground"/>
-                )}
-                <div className="flex items-center justify-between w-full">
-                    <Title initialData={document} />
-                </div>
-            </nav>
-        </>
-     );
+  isCollapsed: boolean;
+  onResetWidth: () => void;
 }
+
+export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
+  const params = useParams();
+
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId as Id<"documents">,
+  });
+
+  if (document === undefined) {
+    return (
+      <nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center">
+        <Title.Skeleton />
+      </nav>
+    );
+  }
+
+  if (document === null) {
+    return null;
+  }
+
+  return (
+    <>
+      <nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center gap-x-4">
+        {isCollapsed && (
+          <MenuIcon
+            role="button"
+            onClick={onResetWidth}
+            className="h-6 w-6 text-muted-foreground"
+          />
+        )}
+        <div className="flex items-center justify-between w-full">
+          <Title initialData={document} />
+        </div>
+      </nav>
+    </>
+  );
+};
